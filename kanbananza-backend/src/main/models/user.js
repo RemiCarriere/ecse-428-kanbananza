f5033f8fd1920fab64a4a0559e4f685d40a1bfb2
Louca Dufault <louca.dufault@gmail.com>
2021-02-12 18:54:54 -0500

    basic schema and mongoose setup

---
+++ kanbananza-backend/src/main/models/user.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  fist_name: { type: String, required: true, maxLength: 100 },
+  last_name: { type: String, required: true, maxLength: 100 },
+});
+
+UserSchema.virtual("name").get(function () {
+  return this.last_name + ", " + this.first_name;
+});
+
+export default mongoose.model("User", UserSchema);
