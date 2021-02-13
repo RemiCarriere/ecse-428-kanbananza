7b4c388840265c5d27515249701fd94f792ebd03
Majd Antaki <majd.antaki@mail.mcgill.ca>
2021-02-13 16:09:45 -0400

    redoing auto styling

--- kanbananza-backend/src/main/app.js
+++ kanbananza-backend/src/main/app.js
@@ -22,7 +22,7 @@ mongoose.connect(databaseUrl, {
 });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "mongoDB connection error:"));
-db.once("open", function () {
+db.once("open", () => {
   console.log("connected to database");
 });
 
