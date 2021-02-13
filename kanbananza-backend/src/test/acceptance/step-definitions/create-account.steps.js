180d0c25c39b17d707cc1ca364249ff7de9c5545
RemiCarriere <remi.carriere@mail.mcgill.ca>
2021-02-13 14:20:16 -0500

    run Prettier

--- kanbananza-backend/src/test/acceptance/step-definitions/create-account.steps.js
+++ kanbananza-backend/src/test/acceptance/step-definitions/create-account.steps.js
@@ -1,74 +1,81 @@
-import { defineFeature, loadFeature } from 'jest-cucumber';
+import { defineFeature, loadFeature } from "jest-cucumber";
 
-const feature = loadFeature('src/test/acceptance/features/Example.feature');
+const feature = loadFeature("src/test/acceptance/features/Example.feature");
 
 defineFeature(feature, (test) => {
   let accountCreated = false;
-  
+
   afterEach(() => {
-      accountCreated = false;
+    accountCreated = false;
   });
 
-  test('Successfully create an account with a valid and unused email (Normal Flow)', ({ given, when, then }) => {
-    given(/^an account with email "(.*)" does not exist in the system$/, (arg0) => {
+  test("Successfully create an account with a valid and unused email (Normal Flow)", ({
+    given,
+    when,
+    then,
+  }) => {
+    given(
+      /^an account with email "(.*)" does not exist in the system$/,
+      (arg0) => {}
+    );
 
-    });
+    when(
+      /^the user attempts to create an account with name "(.*)", email "(.*)", and (.*) "password"$/,
+      (arg0, arg1, arg2) => {}
+    );
 
-    when(/^the user attempts to create an account with name "(.*)", email "(.*)", and (.*) "password"$/, (arg0, arg1, arg2) => {
+    then(
+      /^an account with name "(.*)", email "(.*)", and (.*) "password" shall exist in the system$/,
+      (arg0, arg1, arg2) => {}
+    );
 
-    });
-
-    then(/^an account with name "(.*)", email "(.*)", and (.*) "password" shall exist in the system$/, (arg0, arg1, arg2) => {
-
-    });
-
-    then('the number of accounts in the system shall increase by one', () => {
-
-    });
+    then(
+      "the number of accounts in the system shall increase by one",
+      () => {}
+    );
   });
 
-  test('Unsuccessfully create an account with a valid but existing email (Error Flow)', ({ given, when, then }) => {
-    given(/^an account with email "(.*)" exists in the system$/, (arg0) => {
+  test("Unsuccessfully create an account with a valid but existing email (Error Flow)", ({
+    given,
+    when,
+    then,
+  }) => {
+    given(/^an account with email "(.*)" exists in the system$/, (arg0) => {});
 
-    });
+    when(
+      /^the user attempts to create an account with email "(.*)"$/,
+      (arg0) => {}
+    );
 
-    when(/^the user attempts to create an account with email "(.*)"$/, (arg0) => {
+    then(/^the system shall report "(.*)"$/, (arg0) => {});
 
-    });
+    then(
+      /^an account with email "(.*)" shall exist in the system$/,
+      (arg0) => {}
+    );
 
-    then(/^the system shall report "(.*)"$/, (arg0) => {
-
-    });
-
-    then(/^an account with email "(.*)" shall exist in the system$/, (arg0) => {
-
-    });
-
-    then('the number of accounts shall remain the same', () => {
-
-    });
+    then("the number of accounts shall remain the same", () => {});
   });
 
-  test('Unsuccessfully create an account with an invalid yet unused email (Error Flow)', ({ given, when, then }) => {
-    given('there exist no accounts in the system', () => {
+  test("Unsuccessfully create an account with an invalid yet unused email (Error Flow)", ({
+    given,
+    when,
+    then,
+  }) => {
+    given("there exist no accounts in the system", () => {});
 
-    });
+    when(
+      /^the user attempts to create an account with email (.*)$/,
+      (arg0) => {}
+    );
 
-    when(/^the user attempts to create an account with email (.*)$/, (arg0) => {
+    then(/^the system shall report "Email '(.*)' is invalid'"$/, (arg0) => {});
 
-    });
+    then(
+      /^an account with email "(.*)" shall not exist in the system$/,
+      (arg0) => {}
+    );
 
-    then(/^the system shall report "Email '(.*)' is invalid'"$/, (arg0) => {
-
-    });
-
-    then(/^an account with email "(.*)" shall not exist in the system$/, (arg0) => {
-
-    });
-
-    then('the number of accounts shall remain zero', () => {
-
-    });
+    then("the number of accounts shall remain zero", () => {});
   });
-
 });
