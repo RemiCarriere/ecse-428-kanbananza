e0f6a841d67c386cec2d8f5677e9c23eea69fc50
Majd Antaki <majd.antaki@mail.mcgill.ca>
2021-02-14 14:27:00 -0400

    adding column create + check

---
+++ kanbananza-backend/src/main/services/column.js
@@ -0,0 +1,35 @@
+import { Error } from "mongoose";
+import HttpError from "../http_error";
+import Board from "../models/board";
+import Column from "../models/column";
+
+const createColumn = async ({ label, board }) => {
+  if (!(await Board.exists({ _id: board }))) {
+    throw new HttpError({
+      code: 400,
+      message: `Board with id ${board} does not exist.`,
+    });
+  }
+  if (await Column.exists({ "label": label })) {
+    throw new HttpError({
+      code: 400,
+      message: `Column with label ${newlabel} already exists.`,
+    });
+  }
+  try {
+    const newCol = await Column.insertOne({ label, board });
+    return newCol;
+  } catch (e) {
+    if (e instanceof Error.ValidationError) {
+      throw new HttpError({
+        code: 400,
+        message: "Invalid field(s)",
+        body: Object.values(e.errors).map((error) => error.message),
+      });
+    } else {
+      throw e;
+    }
+  }
+};
+
+export default { createColumn };
