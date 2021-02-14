4709ef9854b78e17c68eb5760a21c5e86907a0b5
Louca Dufault <louca.dufault@gmail.com>
2021-02-13 23:52:24 -0500

    added JSON malformed validation, and user validation based on schema

--- kanbananza-backend/src/main/app.js
+++ kanbananza-backend/src/main/app.js
@@ -5,13 +5,31 @@ import routes from "./routes";
 import errorHandler from "./middleware/error_handler";
 
 import config from "./config";
+import HttpError from "./http_error";
 
 const app = express();
 
 // middleware
 
 // http
-app.use(express.json());
+
+// wrap express.json() to handle JSON SyntaxError
+// see https://stackoverflow.com/a/53049009
+app.use((req, res, next) => {
+  express.json()(req, res, (err) => {
+    if (err) {
+      console.error(err);
+      next(
+        new HttpError({
+          code: 400,
+          message: "Malformed request body JSON.",
+        })
+      );
+    }
+
+    next();
+  });
+});
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
--- kanbananza-backend/src/main/routes/user.js
+++ kanbananza-backend/src/main/routes/user.js
@@ -1,5 +1,6 @@
 import userController from "../controllers/user";
+import { validateSchema } from "../middleware/schema_validator";
 
 export default (router) => {
-  router.post("/user", userController.create);
+  router.post("/user", validateSchema("createUser"), userController.create);
 };
