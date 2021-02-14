46845ccf62d556d27bd375dd92b0e386cffa83fe
Louca Dufault <louca.dufault@gmail.com>
2021-02-14 16:05:12 -0500

    added controller actions mapped to routes, and services for queries

--- kanbananza-backend/src/main/services/board.js
+++ kanbananza-backend/src/main/services/board.js
@@ -2,6 +2,7 @@ import { Error } from "mongoose";
 import HttpError from "../http_error";
 import Board from "../models/board";
 import User from "../models/user";
+import Coumn from "../models/column";
 
 const createBoard = async ({ name, ownerId }) => {
   if (!(await User.exists({ _id: ownerId }))) {
@@ -15,15 +16,63 @@ const createBoard = async ({ name, ownerId }) => {
     return await Board.create({ name, owner: ownerId });
   } catch (e) {
     if (e instanceof Error.ValidationError) {
-      throw new HttpError({
-        code: 400,
-        message: "Invalid field(s)",
-        body: Object.values(e.errors).map((error) => error.message),
-      });
+      next(
+        new HttpError({
+          code: 400,
+          message: "Invalid field(s)",
+          body: Object.values(e.errors).map((error) => error.message),
+        })
+      );
     } else {
-      throw e;
+      next(e);
     }
   }
 };
 
-export default { createBoard };
+const findBoardById = async (id) => {
+  try {
+    return await Board.findOne({ _id: id }).exec();
+  } catch (e) {
+    next(e);
+  }
+};
+
+const findAllBoards = async () => {
+  try {
+    return await Board.find().exec();
+  } catch (e) {
+    next(e);
+  }
+};
+
+const findBoardsByName = async (name) => {
+  try {
+    return await Board.find({ name }).exec();
+  } catch (e) {
+    next(e);
+  }
+};
+
+const findAllBoardColumns = async (id) => {
+  try {
+    return await Column.find({ boardId: id }).exec();
+  } catch (e) {
+    next(e);
+  }
+};
+
+const findAllBoardColumnsByName = async (id, name) => {
+  try {
+    return await Column.find({ boardId: id, name }).exec();
+  } catch (e) {
+    next(e);
+  }
+};
+
+export default {
+  createBoard,
+  findAllBoards,
+  findBoardsByName,
+  findAllBoardColumns,
+  findAllBoardColumnsByName,
+};
