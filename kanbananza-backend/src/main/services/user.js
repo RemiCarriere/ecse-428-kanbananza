4b9e8850bd400e65487bcaacf3f57ab8059b6149
Louca Dufault <louca.dufault@gmail.com>
2021-02-13 21:55:59 -0500

    async bug

--- kanbananza-backend/src/main/services/user.js
+++ kanbananza-backend/src/main/services/user.js
@@ -10,29 +10,27 @@ const createUser = async ({ email, password, firstName, lastName }) => {
     });
   }
 
-  User.create(
-    {
+  try {
+    return await User.create({
       email,
       password,
       first_name: firstName,
       last_name: lastName,
-    },
-    (e, user) => {
-      if (e) {
-        if (e instanceof Error.ValidationError) {
-          throw new HttpError({
-            code: 400,
-            message: "Invalid field(s)",
-            body: Object.values(e.errors).map((error) => error.message),
-          });
-        } else {
-          console.error(e);
-          throw e;
-        }
+    });
+  } catch (e) {
+    if (e) {
+      if (e instanceof Error.ValidationError) {
+        throw new HttpError({
+          code: 400,
+          message: "Invalid field(s)",
+          body: Object.values(e.errors).map((error) => error.message),
+        });
+      } else {
+        console.error(e);
+        throw e;
       }
-      return user;
     }
-  );
+  }
 };
 
 export default { createUser };
