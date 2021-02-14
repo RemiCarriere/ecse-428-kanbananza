d5500d4905a5dec78c991928d92f25e30819ab36
loucadufault <louca.dufault@mail.mcgill.ca>
2021-02-14 14:50:53 -0500

    easier to create http errror from mongoose validation error, works (#65)

--- kanbananza-backend/src/main/services/user.js
+++ kanbananza-backend/src/main/services/user.js
@@ -27,20 +27,7 @@ const createUser = async ({ email, password, firstName, lastName }) => {
     });
   } catch (e) {
     if (e instanceof Error.ValidationError) {
-      throw new HttpError({
-        code: 400,
-        message: `Invalid field${
-          Object.values(e.errors).length > 1 ? "s" : ""
-        }.`,
-        errors: Object.values(e.errors).map(
-          (error) =>
-            new ValidationError({
-              path: error.path,
-              reason: error.message,
-              data: error.value,
-            })
-        ),
-      });
+      throw HttpError.fromMongooseValidationError(e);
     } else {
       throw e;
     }
