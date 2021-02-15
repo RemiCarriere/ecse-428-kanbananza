06ae4fa20088de3dea04fa084d5096e35d55de65
Louca Dufault <louca.dufault@gmail.com>
2021-02-14 20:00:34 -0500

    dfomart

--- kanbananza-backend/src/test/acceptance/step-definitions/add-card-to-column.steps.js
+++ kanbananza-backend/src/test/acceptance/step-definitions/add-card-to-column.steps.js
@@ -1,56 +1,39 @@
 import { defineFeature, loadFeature } from "jest-cucumber";
 import { systemShallReport } from "./background.steps";
 
-const feature = loadFeature("src/test/acceptance/features/ID005_Add_A_Card_To_A_Column.feature");
+const feature = loadFeature(
+  "src/test/acceptance/features/ID005_Add_A_Card_To_A_Column.feature"
+);
 
 const givenUserLoggedIn = (given) => {
-
-  given('user with username Fizbin is logged in', () => {
-
-  });
-
+  given("user with username Fizbin is logged in", () => {});
 };
 
 const givenUserHasOneBoard = (given) => {
-
-  given('the user has one board', () => {
-
-  });
-
+  given("the user has one board", () => {});
 };
 
 const givenBoardIsSelected = (given) => {
-
-  given('the user has selected that board', () => {
-
-  });
-
+  given("the user has selected that board", () => {});
 };
 
 const givenBoardHasOneColumn = (given) => {
-
-  given(/^the board has one column with name "(.*)"$/, (name) => {
-
-  });
-
+  given(/^the board has one column with name "(.*)"$/, (name) => {});
 };
 
 const whenUserAddsCard = (when) => {
-
-  when(/^the user attempts to add a card with name "(.*)" to the column with name "(.*)"$/, (card, column) => {
-
-  });
-
+  when(
+    /^the user attempts to add a card with name "(.*)" to the column with name "(.*)"$/,
+    (card, column) => {}
+  );
 };
 
 const thenCardsInColumnIncreaseByOne = (then) => {
-
-  then(/^the number of cards included in the column with name "(.*)" shall increase by one$/, (name) => {
-
-  });
-
+  then(
+    /^the number of cards included in the column with name "(.*)" shall increase by one$/,
+    (name) => {}
+  );
 };
-
 
 defineFeature(feature, (test) => {
   let cardAdded = false;
@@ -59,12 +42,11 @@ defineFeature(feature, (test) => {
     cardAdded = false;
   });
 
-  test('Successfully add a card with a valid but existing name to an existing column (Alternate Flow)', 
-  ({ given, 
-    when, 
-    then
+  test("Successfully add a card with a valid but existing name to an existing column (Alternate Flow)", ({
+    given,
+    when,
+    then,
   }) => {
-
     givenUserLoggedIn(given);
 
     givenUserHasOneBoard(given);
@@ -73,56 +55,64 @@ defineFeature(feature, (test) => {
 
     givenBoardHasOneColumn(given);
 
-    given(/^the column with name "(.*)" includes an existing card with name "(.*)"$/, (column, name) => {
-
-    });
+    given(
+      /^the column with name "(.*)" includes an existing card with name "(.*)"$/,
+      (column, name) => {}
+    );
 
     whenUserAddsCard(when);
 
-    then(/^two cards with name "(.*)" shall be included in the column with name "(.*)"$/, (card, column) => {
-
-    });
+    then(
+      /^two cards with name "(.*)" shall be included in the column with name "(.*)"$/,
+      (card, column) => {}
+    );
 
     thenCardsInColumnIncreaseByOne(then);
 
-    then('the number of cards in the board shall increase by one', () => {
-
-    });
+    then("the number of cards in the board shall increase by one", () => {});
   });
 
-  test('Unsuccessfully add a card with an invalid name comprising only whitespace to an existing column (Error Flow)', ({ given, when, then, and }) => {
-
+  test("Unsuccessfully add a card with an invalid name comprising only whitespace to an existing column (Error Flow)", ({
+    given,
+    when,
+    then,
+    and,
+  }) => {
     givenUserLoggedIn(given);
 
     givenUserHasOneBoard(given);
 
     givenBoardIsSelected(given);
 
-    givenBoardHasOneColumn(given);;
+    givenBoardHasOneColumn(given);
 
-    given(/^the column with name "(.*)" includes no existing cards$/, (name) => {
-
-    });
+    given(
+      /^the column with name "(.*)" includes no existing cards$/,
+      (name) => {}
+    );
 
     whenUserAddsCard(when);
 
     systemShallReport(then);
 
-    then(/^a card with name "(.*)" shall not be included in the column with name "(.*)"$/, (card, column) => {
+    then(
+      /^a card with name "(.*)" shall not be included in the column with name "(.*)"$/,
+      (card, column) => {}
+    );
 
-    });
+    then(
+      /^the number of cards included in the column with name "(.*)" shall remain zero$/,
+      (name) => {}
+    );
 
-    then(/^the number of cards included in the column with name "(.*)" shall remain zero$/, (name) => {
-
-    });
-
-    then('the number of cards in the board shall remain the same', () => {
-
-    });
+    then("the number of cards in the board shall remain the same", () => {});
   });
 
-  test('Successfully add a card with a valid name to an existing column (Normal Flow)', ({ given, when, then}) => {
-
+  test("Successfully add a card with a valid name to an existing column (Normal Flow)", ({
+    given,
+    when,
+    then,
+  }) => {
     givenUserLoggedIn(given);
 
     givenUserHasOneBoard(given);
@@ -131,21 +121,23 @@ defineFeature(feature, (test) => {
 
     givenBoardHasOneColumn(given);
 
-    given(/^the column with name "(.*)" does not include an existing card with name (.*)$/, (column, card) => {
-
-    });
+    given(
+      /^the column with name "(.*)" does not include an existing card with name (.*)$/,
+      (column, card) => {}
+    );
 
     whenUserAddsCard(when);
 
-    then(/^one card with name (.*) shall be included in the column with name "(.*)"$/, (card, column) => {
-
-    });
+    then(
+      /^one card with name (.*) shall be included in the column with name "(.*)"$/,
+      (card, column) => {}
+    );
 
     thenCardsInColumnIncreaseByOne(then);
 
-    then('the number of cards in the board shall increase by one', (table) => {
-
-    });
-});
-
+    then(
+      "the number of cards in the board shall increase by one",
+      (table) => {}
+    );
+  });
 });
--- kanbananza-backend/src/test/acceptance/step-definitions/create-board.steps.js
+++ kanbananza-backend/src/test/acceptance/step-definitions/create-board.steps.js
@@ -1,20 +1,22 @@
 import { defineFeature, loadFeature } from "jest-cucumber";
 import { givenUserIsLoggedIn, systemShallReport } from "./background.steps";
 
-const feature = loadFeature("src/test/acceptance/features/ID002_Create_Board.feature");
+const feature = loadFeature(
+  "src/test/acceptance/features/ID002_Create_Board.feature"
+);
 
 const whenUserCreatesBoardWithName = (when) => {
   when(
-    /^the logged-in user attempts to create a new board with name "(.*)"$/,(name) => {
-
-    });
+    /^the logged-in user attempts to create a new board with name "(.*)"$/,
+    (name) => {}
+  );
 };
 
 const userShallHaveBoardWithName = (then) => {
   then(
-    /^the logged-in user shall have a board with name "(.*)"$/, (name) => {
-
-    });
+    /^the logged-in user shall have a board with name "(.*)"$/,
+    (name) => {}
+  );
 };
 
 defineFeature(feature, (test) => {
@@ -29,13 +31,11 @@ defineFeature(feature, (test) => {
     when,
     then,
   }) => {
-
     givenUserIsLoggedIn(given);
 
     given(
-      /^the logged-in user has no existing boards with name "(.*)"$/, (name) => {
-
-      }
+      /^the logged-in user has no existing boards with name "(.*)"$/,
+      (name) => {}
     );
 
     whenUserCreatesBoardWithName(when);
@@ -43,9 +43,8 @@ defineFeature(feature, (test) => {
     userShallHaveBoardWithName(then);
 
     then(
-      /^the logged-in user shall be authorized to view the board with name "(.*)"$/, (name) => {
-
-      }
+      /^the logged-in user shall be authorized to view the board with name "(.*)"$/,
+      (name) => {}
     );
 
     then(
@@ -59,26 +58,28 @@ defineFeature(feature, (test) => {
     when,
     then,
   }) => {
-
     givenUserIsLoggedIn(given);
 
-    given(/^the logged-in user has an existing board with name "(.*)"$/, (name) => {
+    given(
+      /^the logged-in user has an existing board with name "(.*)"$/,
+      (name) => {}
+    );
 
-    });
-
-    whenUserCreatesBoardWithName(when)
+    whenUserCreatesBoardWithName(when);
 
     systemShallReport(then);
 
     userShallHaveBoardWithName(then);
 
     then(
-      /^the logged-in user shall be authorized to view the board with name "(.*)"$/, (name) => {
-
-      }
+      /^the logged-in user shall be authorized to view the board with name "(.*)"$/,
+      (name) => {}
     );
 
-    then("the number of boards the logged-in user has shall remain the same", () => {});
+    then(
+      "the number of boards the logged-in user has shall remain the same",
+      () => {}
+    );
   });
 
   test("Unsuccessfully create a board with an invalid name comprising only whitespace (Error Flow)", ({
@@ -86,23 +87,22 @@ defineFeature(feature, (test) => {
     when,
     then,
   }) => {
-
     givenUserIsLoggedIn(given);
 
-    given(/^the logged-in user has no existing boards$/, () => {
+    given(/^the logged-in user has no existing boards$/, () => {});
 
-    });
-
-    whenUserCreatesBoardWithName(when)
+    whenUserCreatesBoardWithName(when);
 
     systemShallReport(then);
 
-    then(/^the logged-in user shall not have a board with name "(.*)"$/, (name) => {
+    then(
+      /^the logged-in user shall not have a board with name "(.*)"$/,
+      (name) => {}
+    );
 
-    });
-
-    then("the number of boards the logged-in user has shall remain zero", () => {
-
-    });
+    then(
+      "the number of boards the logged-in user has shall remain zero",
+      () => {}
+    );
   });
 });
