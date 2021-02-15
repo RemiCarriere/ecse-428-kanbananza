518313a40568427d8ec7e5c3091e7eeb032ff954
RemiCarriere <36647230+RemiCarriere@users.noreply.github.com>
2021-02-14 21:44:32 -0500

    user endpoiints (#73)

--- kanbananza-backend/src/main/controllers/user.js
+++ kanbananza-backend/src/main/controllers/user.js
@@ -1,6 +1,7 @@
 import userService from "../services/user";
 import UserDTO from "../DTO/user";
 import auth from "../auth";
+import BoardDTO from "../DTO/board";
 
 const create = async (req, res, next) => {
   try {
@@ -39,4 +40,33 @@ const login = async (req, res, next) => {
   }
 };
 
-export default { create, findByEmail, login };
+const index = async (req, res, next) => {
+  try {
+    const user = await userService.findUserById(req.params.id);
+    res.status(200).json(UserDTO.fromDocument(user));
+  } catch (e) {
+    next(e);
+  }
+};
+
+const select = async (req, res, next) => {
+  let users = [];
+  try {
+    users = await userService.findAllUsers();
+  } catch (e) {
+    next(e);
+  }
+  res.status(200).json(users.map((user) => UserDTO.fromDocument(user)));
+};
+
+const selectBoards = async (req, res, next) => {
+  let boards = [];
+  try {
+    boards = await userService.findAllUserBoards(req.params.id);
+  } catch (e) {
+    next(e);
+  }
+  res.status(200).json(boards.map((board) => BoardDTO.fromDocument(board)));
+};
+
+export default { create, findByEmail, login, index, select, selectBoards };
--- kanbananza-backend/src/main/routes/user.js
+++ kanbananza-backend/src/main/routes/user.js
@@ -1,15 +1,15 @@
 import basicAuth from "express-basic-auth";
 import auth from "../auth";
 import userController from "../controllers/user";
+import idParamValidator from "../middleware/id_param_validator";
 import { validateSchema } from "../middleware/schema_validator";
 
 export default (router) => {
   router.post("/user", validateSchema("createUser"), userController.create);
-  router.get(
-    "/user",
-    basicAuth({ authorizer: auth.authAll }),
-    userController.findByEmail
-  );
+  router.get("/user", userController.findByEmail);
+  router.get("/user/:id", idParamValidator, userController.index);
+  router.get("/user/:id/boards", idParamValidator, userController.selectBoards);
+  router.get("/users/", userController.select);
   router.get(
     "/login",
     basicAuth({ authorizer: auth.authAll }),
