f0f63977294e45d2d38e170627dcb53a60cbf33a
RemiCarriere <remi.carriere@mail.mcgill.ca>
2021-02-14 21:39:45 -0500

    updating model

--- kanbananza-backend/src/main/models/column.js
+++ kanbananza-backend/src/main/models/column.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 const columnSchema = new Schema({
   label: { type: String, required: true, maxLength: 100 },
   boardId: { type: Schema.ObjectId, required: true, ref: "Board" },
+  order : {type: Number, required: true}
 });
 
 export default mongoose.model("Column", columnSchema);
