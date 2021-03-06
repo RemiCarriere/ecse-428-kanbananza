adfd6c16326f33cbd02ae07482596c0e2dc83a9f
And7ew <46793409+And7ew@users.noreply.github.com>
2021-03-06 17:11:44 -0500

    error features for login

--- kanbananza-frontend/src/components/Login/Login.tsx
+++ kanbananza-frontend/src/components/Login/Login.tsx
@@ -3,31 +3,37 @@ import { createLogin } from "../../api/userApi";
 import { useHistory } from "react-router-dom";
 import Cookies from "js-cookie";
 import { propTypes } from "react-bootstrap/esm/Image";
+import $ from "jquery";
 
 const Login = (params) => {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const history = useHistory();
+  var error;
 
   async function onLogin(event: any) {
-    try {
+  
       const res = await createLogin(email, password);
       if (res && res.token) {
         Cookies.set("token", res.token);
         params.setloggedIn(true);
         history.push("/home");
+      } else{
+       $("#error").text("Wrong Email or Password!");
+       $("#error").css("color", "red");
+       console.log("here");
       }
-    } catch (e) {
-      console.log(e);
-    }
-  }
+    } 
+  
 
   return (
     <div className="auth-wrapper">
-      s
+      
       <div className="auth-inner">
         <form>
           <h3>Sign In</h3>
+
+          <span id = "error"></span>
 
           <div className="form-group">
             <label>Email address</label>
