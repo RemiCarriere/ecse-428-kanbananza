3d01a510c2a9d11f9050574e10c5e123937d4d31
RemiCarriere <36647230+RemiCarriere@users.noreply.github.com>
2021-03-06 08:39:14 -0500

    prelim delete col test (#101)
    * prelim delete col test
    
    * fix style
    
    * Louca/delete column (#100)
    
    * add cascading resource deletion for column and board
    
    * relax linting, fixes
    
    * format
    
    * enforce order uniqueness in column (#102)
    
    * enforce order uniqueness in column
    
    * fix acceptance test
    
    * prelim delete col test
    
    * fix style
    
    * fix async table step
    
    * fix async table step
    
    * complete delete col test
    
    Co-authored-by: loucadufault <louca.dufault@mail.mcgill.ca>

--- kanbananza-backend/src/test/acceptance/features/ID0010-delete-a-column.feature
+++ kanbananza-backend/src/test/acceptance/features/ID0010-delete-a-column.feature
@@ -3,7 +3,7 @@ Feature: 010 - Delete a Column
 As a user I want to be able to delete a column from the board
 Background:
 Given user with username Fizbin is logged in
-Given the user owns one board
+Given the user has one board
 Given the user has selected that board
 Given the selected board has three columns ordered as follows:
 | columnName | columnOrder |
@@ -11,26 +11,21 @@ Given the selected board has three columns ordered as follows:
 | Doing      | 2           |
 | Done       | 3           |
 
-
-Scenario Outline: Delete a column from the board (Normal Flow)
-When the user attempts to delete column with name <columnName>
-Then the board will only contain two columns
-Examples:
-| columnName | 
-| To Do      | 
-| Doing	     | 
-| Done       | 
-
-
-Scenario: Delete a column from a board with one column (Alternate Flow)
-Given the selected board has only one column with name "TODO"
-When the user attempts to delete column with name "TODO"
-Then a warning "are you sure you want to procede with this operation? A board without columns will be deleted" will be issued
+Scenario: Delete a column from the board (Normal Flow)
+When the user attempts to delete column with name "To Do"
+Then the board will have the following columns:
+| columnName | columnOrder |
+| Doing      | 1           |
+| Done       | 2           |
 
 Scenario: Delete column that does not exist (Error Flow)
 When the user attempts to delete column with name "non existant name"
 Then an error "The column does not exist" is issued
-
+Then the board will have the following columns:
+| columnName | columnOrder |
+| To Do      | 1           |
+| Doing      | 2           |
+| Done       | 3           |
 
 
 
--- kanbananza-backend/src/test/acceptance/step-definitions/delete-column.steps.js
+++ kanbananza-backend/src/test/acceptance/step-definitions/delete-column.steps.js
@@ -4,76 +4,118 @@ import request from "../../support/request";
 const feature = loadFeature(
   "src/test/acceptance/features/ID0010-delete-a-column.feature"
 );
-const errMsg = "";
-let responseStatus = "";
+let errMsg = "";
+let userID = "";
+let selectedBoard = "";
 
-//TODO: Implement the step definitions and remove .skip
+const givenFizBinLoggedIn = (given) => {
+  given("user with username Fizbin is logged in", async () => {
+    await request.post("/user").send({
+      email: "fiz@bin.com",
+      firstName: "fiz",
+      lastName: "bin",
+      password: "password",
+    });
+    const res = await request
+      .post("/login")
+      .send({ email: "fiz@bin.com", password: "password" });
+    userID = res.body.id;
+  });
+};
+
+const givenUserHasOneBoard = (given) => {
+  given("the user has one board", async () => {
+    const res = await request
+      .post("/board")
+      .send({ ownerId: userID, name: "test-board-name" });
+    selectedBoard = res.body.id;
+  });
+};
+
+const givenBoardSelected = (given) => {
+  given("the user has selected that board", () => {
+    selectedBoard = selectedBoard; //rendundant step for our implementation
+  });
+};
+
+const givenBoardHasFollowingColumns = (given) => {
+  given(
+    "the selected board has three columns ordered as follows:",
+    async (table) => {
+      for (const row of table) {
+        await request.post("/column").send({
+          name: row.columnName,
+          boardId: selectedBoard,
+          order: parseInt(row.columnOrder),
+        });
+      }
+    }
+  );
+};
+
+const whenUserAttemptsToDeleteColumn = (when) => {
+  when(
+    /^the user attempts to delete column with name "(.*)"$/,
+    async (colName) => {
+      const res = await request.get(`/board/${selectedBoard}/columns`);
+      let colId = res.body.filter((column) => column.name === colName)[0];
+      if (colId) {
+        colId = colId.id;
+      } else {
+        colId = "non-existent column";
+      }
+      const res1 = await request.delete(`/column/${colId}`);
+      if (res1.body) {
+        errMsg = res1.body.errors;
+      }
+    }
+  );
+};
+
 defineFeature(feature, (test) => {
-  test.skip("Delete a column from a board with one column (Alternate Flow)", ({
+  test("Delete column that does not exist (Error Flow)", ({
     given,
     when,
     then,
   }) => {
-    given("user with username Fizbin is logged in", () => {});
+    givenFizBinLoggedIn(given);
 
-    given("the user owns one board", () => {});
+    givenUserHasOneBoard(given);
 
-    given("the user has selected that board", () => {});
+    givenBoardSelected(given);
 
-    given(
-      "the selected board has three columns ordered as follows:",
-      (table) => {}
-    );
+    givenBoardHasFollowingColumns(given);
 
-    given(
-      /^the selected board has only one column with name "(.*)"$/,
-      (arg0) => {}
-    );
+    whenUserAttemptsToDeleteColumn(when);
 
-    when(/^the user attempts to delete column with name "(.*)"$/, (arg0) => {});
+    then(/^an error "(.*)" is issued$/, (error) => {
+      expect(errMsg).not.toBe("");
+    });
 
-    then(/^a warning "(.*)" will be issued$/, (arg0) => {});
+    then("the board will have the following columns:", async (table) => {
+      const res = await request.get(`/board/${selectedBoard}/columns`);
+      expect(res.body.length).toBe(table.length);
+    });
   });
 
-  test.skip("Delete column that does not exist (Error Flow)", ({
+  test("Delete a column from the board (Normal Flow)", ({
     given,
     when,
     then,
   }) => {
-    given("user with username Fizbin is logged in", () => {});
+    givenFizBinLoggedIn(given);
 
-    given("the user owns one board", () => {});
+    givenUserHasOneBoard(given);
 
-    given("the user has selected that board", () => {});
+    givenBoardSelected(given);
 
-    given(
-      "the selected board has three columns ordered as follows:",
-      (table) => {}
-    );
+    givenBoardHasFollowingColumns(given);
 
-    when(/^the user attempts to delete column with name "(.*)"$/, (arg0) => {});
+    whenUserAttemptsToDeleteColumn(when);
 
-    then(/^an error "(.*)" is issued$/, (arg0) => {});
-  });
-
-  test.skip("Delete a column from the board (Normal Flow)", ({
-    given,
-    when,
-    then,
-  }) => {
-    given("user with username Fizbin is logged in", () => {});
-
-    given("the user owns one board", () => {});
-
-    given("the user has selected that board", () => {});
-
-    given(
-      "the selected board has three columns ordered as follows:",
-      (table) => {}
-    );
-
-    when(/^the user attempts to delete column with name (.*)$/, (arg0) => {});
-
-    then("the board will only contain two columns", () => {});
+    then("the board will have the following columns:", async (table) => {
+      const res = await request.get(`/board/${selectedBoard}/columns`);
+      expect(res.body.length).toBe(table.length);
+    });
   });
 });
