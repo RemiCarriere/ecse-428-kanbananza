df1d730fe00ac9ee1156a0063ae24aa9975649d0
And7ew <46793409+And7ew@users.noreply.github.com>
2021-03-06 17:12:14 -0500

    updates signup errors message

--- kanbananza-frontend/src/components/Login/Signup.tsx
+++ kanbananza-frontend/src/components/Login/Signup.tsx
@@ -2,31 +2,49 @@ import { useState } from "react";
 import Cookies from "js-cookie";
 import { createUser } from "../../api/userApi";
 import { useHistory } from "react-router-dom";
+import $ from "jquery";
+import { Check } from "@material-ui/icons";
 
 const Signup = (params) => {
   const [email, setEmail] = useState<string>("");
   const [firstname, setFirstname] = useState<string>("");
   const [lastname, setLastname] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [confirmpassword, setconfirmPassword] = useState<string>("");
   const history = useHistory();
+  var error;
+  var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
 
   async function onLogin(event: any) {
-    try {
-      const res = await createUser(firstname, lastname, email, password);
-      if (res && res.token) {
-        Cookies.set("token", res.token);
-        params.setloggedIn(true);
-        history.push("/home");
+
+    //need to add error if user account is already created
+    
+      if (password !== confirmpassword){
+        $("#error").text("Passwords Do Not Match!");
+        $("#error").css("color", "red");
       }
-    } catch (e) {
-      console.log(e);
-    }
+      if (!pattern.test(email)){
+        $("#error1").text("Please Enter A Valid Email!");
+        $("#error1").css("color", "red");
+      }
+      else{
+        const res = await createUser(firstname, lastname, email, password);
+      
+        if (res && res.token) {
+          Cookies.set("token", res.token);
+          params.setloggedIn(true);
+          history.push("/home");
+        } 
+      }
   }
+  
   return (
     <div className="auth-wrapper">
       <div className="auth-inner">
         <form>
           <h3>Sign Up</h3>
+
+          
 
           <div className="form-group">
             <label>First name</label>
@@ -48,6 +66,8 @@ const Signup = (params) => {
             />
           </div>
 
+          <span id = "error1"></span>
+
           <div className="form-group">
             <label>Email address</label>
             <input
@@ -58,6 +78,8 @@ const Signup = (params) => {
             />
           </div>
 
+          <span id = "error"></span>
+
           <div className="form-group">
             <label>Password</label>
             <input
@@ -65,6 +87,16 @@ const Signup = (params) => {
               className="form-control"
               placeholder="Enter password"
               onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Confirm Password</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Confirm password"
+              onChange={(e) => setconfirmPassword(e.target.value)}
             />
           </div>
 
