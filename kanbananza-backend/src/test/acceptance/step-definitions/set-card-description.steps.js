c079efe9fa8b3146d0591c7084d9a6fa40902aea
celine-nader <celine.nader1@gmail.com>
2021-03-06 20:46:18 -0500

    acceptance test defs for set priority of card and set description of card.

--- kanbananza-backend/src/test/acceptance/step-definitions/set-card-description.steps.js
+++ kanbananza-backend/src/test/acceptance/step-definitions/set-card-description.steps.js
@@ -6,110 +6,182 @@ const feature = loadFeature(
 );
 const errMsg = "";
 let responseStatus = "";
+let userID = "";
+let selectedBoard = "";
+let colID = "";
 
-//TODO: Implement the step definitions and remove .skip
+
+const givenFizBinLoggedIn = (given) => {
+  given("user with username Fizbin is logged into the system", async () => {
+    await request.post("/user").send({
+      email: "fiz@bin.com",
+      firstName: "fiz",
+      lastName: "bin",
+      password: "password",
+    });
+    const res = await request
+      .post("/login")
+      .send({ email: "fiz@bin.com", password: "password" });
+    userID = res.body.id;
+  });
+};
+
+const givenUserHasOneBoard = (given) => {
+  given("the user has one board", async () => {
+    const res = await request
+      .post("/board")
+      .send({ ownerId: userID, name: "test-board-name" });
+    selectedBoard = res.body.id;
+  });
+};
+
+const givenBoardSelected = (given) => {
+  given("the user has selected that board", () => {
+    selectedBoard = selectedBoard; //rendundant step for our implementation
+  });
+};
+
+const givenBoardHasColumnWithName = (given) => {
+  given(/^the board has one column with name "(.*)"$/, async (colName) => {
+    const res = await request
+      .post("/column")
+      .send({ name: colName, boardId: selectedBoard, order: 1 });
+    colID = res.body.id;
+  });
+};
+
+const givenColumnHasOneCardWithName = (given) => {
+  given(
+    /^that column has one and only one card with name "(.*)"$/,
+    async(colName, name) => {
+      const { body } = await request
+      .get(`/column/${colID}/cards`)
+      .expect(200);
+
+    expect(body[0].name).toBe(name);
+    
+    });
+};
+
+const givenCardShallHaveThatDescription = (given) => {
+  given(
+    /^the card with name "(.*)" has a description "(.*)"$/,
+    async (cardName, cardDescription) => {
+      const { body } = await request
+      .get(`/column/${colID}/cards/`) //I think this has an error
+      .expect(200);
+
+    expect(body[0].description).toBe(cardDescription); 
+    }
+  );
+};
+
+const givenCardHasNullDescription = (given) => { //Check that, could have an error here 
+  given(
+    /^the card with name "(.*)" does not have a description$/,
+    async (cardName, NULL ) => {
+      const { body } = await request
+      .get(`/column/${colID}/cards/`) //I think this has an error
+      .expect(200);
+
+    expect(body[0].description).toBe(NULL); 
+    }
+  );
+};
+
+const whenUserSetsDescriptionOfCard = (when) => {
+  when(
+    /^the user sets the description of the card with name "(.*)" to "(.*)"$/,
+    async (cardName, cardDescription) => {
+      const res = await request
+      .put("/card") //should you use put here? How do you get the specific card with cardName??
+      .set({description: cardDescription});
+    }
+  );
+};
+
+
+//Should that be different than the one above? Two methods practically the same, one with given and the other with then
+const thenCardShallHaveThatDescription = (then) => {
+  then(
+    /^the card with name "(.*)" has a description "(.*)"$/,
+    async (cardName, cardDescription) => {
+      const { body } = await request
+      .get(`/column/${colID}/cards/`) //I think this has an error
+      .expect(200);
+
+    expect(body[0].description).toBe(cardDescription); 
+    }
+  );
+};
+
+
 defineFeature(feature, (test) => {
-  test.skip("Set a valid description for a card with an existing description (Alternate Flow)", ({
+  test("Set a valid description for a card with an existing description (Alternate Flow)", ({
     given,
     when,
     then,
   }) => {
-    given("user with username Fizbin is logged into the system", () => {});
+    givenFizBinLoggedIn(given); //Remy
 
-    given("the user has one board", () => {});
+    givenUserHasOneBoard(given); //Remy
 
-    given("the user has selected that board", () => {});
+    givenBoardSelected(given); // Remy
 
-    given(/^the board has one column with name "(.*)"$/, (arg0) => {});
+    givenBoardHasColumnWithName(given); // Remy
 
-    given(
-      /^that column has one and only one card with name "(.*)"$/,
-      (arg0) => {}
-    );
+    givenColumnHasOneCardWithName(given); //I implemented this
 
-    given(
-      /^the card with name "(.*)" has a description "(.*)"$/,
-      (arg0, arg1) => {}
-    );
+    givenCardShallHaveThatDescription(given); // I implemented this
 
-    when(
-      /^the user sets the description of the card with name "(.*)" to "(.*)"$/,
-      (arg0, arg1) => {}
-    );
+    whenUserSetsDescriptionOfCard(when); // I implemented this
 
-    then(
-      /^the card with name "(.*)" has description "(.*)"$/,
-      (arg0, arg1) => {}
-    );
+    thenCardShallHaveThatDescription(then); //I implemented this 
   });
 
-  test.skip("Set the description for a card with no description to whitespace (Error Flow)", ({
+  test("Set the description for a card with no description to whitespace (Error Flow)", ({
     given,
     when,
     then,
   }) => {
-    given("user with username Fizbin is logged into the system", () => {});
+    givenFizBinLoggedIn(given); //Took it from previous files
 
-    given("the user has one board", () => {});
+    givenUserHasOneBoard(given); //Took it from previous files
 
-    given("the user has selected that board", () => {});
+    givenBoardSelected(given); //Took it from previous files
 
-    given(/^the board has one column with name "(.*)"$/, (arg0) => {});
+    givenBoardHasColumnWithName(given); //Took it from previous files
 
-    given(
-      /^that column has one and only one card with name "(.*)"$/,
-      (arg0) => {}
-    );
+    givenColumnHasOneCardWithName(given) //I implemented this
 
-    given(
-      /^the card with name "(.*)" has a description "(.*)"$/,
-      (arg0, arg1) => {}
-    );
+    givenCardShallHaveThatDescription(given)
 
-    when(
-      /^the user sets the description of the card with name "(.*)" to "(.*)"$/,
-      (arg0, arg1) => {}
-    );
+    whenUserSetsDescriptionOfCard(when);
 
-    then(/^a message "(.*)" is issued$/, (arg0) => {});
+    then(/^a message "(.*)" is issued$/, (arg0) => {}); //Do that part. 
 
-    then(
-      /^the card with name "(.*)" has description "(.*)"$/,
-      (arg0, arg1) => {}
-    );
+    thenCardShallHaveThatDescription(then); //I implemented this 
   });
 
-  test.skip("Set a valid description for a card with no description (Normal Flow)", ({
+  test("Set a valid description for a card with no description (Normal Flow)", ({
     given,
     when,
     then,
   }) => {
-    given("user with username Fizbin is logged into the system", () => {});
+    givenFizBinLoggedIn(given);
 
-    given("the user has one board", () => {});
+    givenUserHasOneBoard(given);
 
-    given("the user has selected that board", () => {});
+    givenBoardSelected(given);
 
-    given(/^the board has one column with name "(.*)"$/, (arg0) => {});
+    givenBoardHasColumnWithName(given);
 
-    given(
-      /^that column has one and only one card with name "(.*)"$/,
-      (arg0) => {}
-    );
+    givenColumnHasOneCardWithName(given); //I implemented this
 
-    given(
-      /^the card with name "(.*)" does not have a description$/,
-      (arg0) => {}
-    );
+    givenCardHasNullDescription(given); //I implemented this. Check for errors. 
 
-    when(
-      /^the user sets the description of the card with name "(.*)" to (.*)$/,
-      (arg0, arg1) => {}
-    );
+    whenUserSetsDescriptionOfCard(when); // I implemented this. Check for errors.
 
-    then(
-      /^the card with name "(.*)" has description (.*)$/,
-      (arg0, arg1) => {}
-    );
+    thenCardShallHaveThatDescription(then); //I implemented this. Check for errors. 
   });
 });
