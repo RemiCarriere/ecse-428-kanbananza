876aa3dfa3118007403ada72712d52ddbb87e273
mattcaccavelli <matt.caccavelli@gmail.com>
2021-03-07 21:20:56 -0500

    almost working

--- kanbananza-backend/src/test/acceptance/step-definitions/set-card-priority.steps.js
+++ kanbananza-backend/src/test/acceptance/step-definitions/set-card-priority.steps.js
@@ -9,6 +9,7 @@ let responseStatus = "";
 let userID = "";
 let selectedBoard = "";
 let colID = "";
+let cardId;
 
 const givenFizBinLoggedIn = (given) => {
   given("user with username Fizbin is logged into the system", async () => {
@@ -52,10 +53,13 @@ const givenBoardHasColumnWithName = (given) => {
 const givenColumnHasOneCardWithName = (given) => {
   given(
     /^that column has one and only one card with name "(.*)"$/,
-    async (colName, name) => {
-      const { body } = await request.get(`/column/${colID}/cards`).expect(200);
+    async (name) => {
+      const req = await request.post("/card")
+      .send({ name: name, columnId: colID, order: 1})
+      .expect(201);
 
-      expect(body[0].name).toBe(name);
+      cardId = req.body.id;
+      console.log(cardId);
     }
   );
 };
@@ -65,10 +69,9 @@ const givenCardShallHaveThatPriority = (given) => {
     /^the card with name "(.*)" has priority "(.*)"$/,
     async (cardName, cardPriority) => {
       const { body } = await request
-        .get(`/column/${colID}/cards/`) //I think this has an error
+        .put(`/card/${cardId}`)
+        .send({priority: cardPriority}) //I think this has an error
         .expect(200);
-
-      expect(body[0].priority).toBe(cardPriority);
     }
   );
 };
@@ -78,8 +81,9 @@ const whenUserSetsPriorityOfCard = (when) => {
     /^the user sets the priority of the card with name "(.*)" to (.*)$/,
     async (cardName, cardPriority) => {
       const res = await request
-        .put("/card") //should you use put here? How do you get the specific card with cardName??
-        .set({ priority: cardPriority });
+        .put(`/cards/${cardId}`)
+        .set({ priority: cardPriority })
+        .expect(200);
     }
   );
 };
@@ -87,7 +91,7 @@ const whenUserSetsPriorityOfCard = (when) => {
 //Should that be different than the one above? Two methods practically the same, one with given and the other with then
 const thenCardShallHaveThatPriority = (then) => {
   then(
-    /^the card with name "(.*)" has priority "(.*)"$/,
+    /^the card with name "(.*)" has priority (.*)$/,
     async (cardName, cardPriority) => {
       const { body } = await request
         .get(`/column/${colID}/cards/`) //I think this has an error
@@ -107,71 +111,54 @@ const givenCardHasNullPriority = (given) => {
         .get(`/column/${colID}/cards/`) //I think this has an error
         .expect(200);
 
-      expect(body[0].priority).toBe(NULL);
+      expect(body[0].priority).toBe(undefined);
     }
   );
 };
 //TODO: Implement the step definitions and remove .skip
 defineFeature(feature, (test) => {
-  test.skip("Set a priority for a card with an existing priority (Alternate Flow)", ({
+  test("Set a priority for a card with an existing priority (Alternate Flow)", ({
     given,
     when,
     then,
   }) => {
-    given("user with username Fizbin is logged into the system", () => {});
+    
+    givenFizBinLoggedIn(given);
 
-    given("the user has one board", () => {});
+    givenUserHasOneBoard(given);
 
-    given("the user has selected that board", () => {});
+    givenBoardSelected(given);
 
-    given(/^the board has one column with name "(.*)"$/, (arg0) => {});
+    givenBoardHasColumnWithName(given);
 
-    given(
-      /^that column has one and only one card with name "(.*)"$/,
-      (arg0) => {}
-    );
+    givenColumnHasOneCardWithName(given);
 
-    given(
-      /^the card with name "(.*)" has priority "(.*)"$/,
-      (arg0, arg1) => {}
-    );
+    givenCardShallHaveThatPriority(given);
 
-    when(
-      /^the user sets the priority of card with name "(.*)" to "(.*)"$/,
-      (arg0, arg1) => {}
-    );
+    whenUserSetsPriorityOfCard(when);
 
-    then(/^the card with name "(.*)" has priority "(.*)"$/, (arg0, arg1) => {});
+    thenCardShallHaveThatPriority(then);
   });
 
-  test.skip("Set a priority for a card with no priority (Normal Flow)", ({
+  test("Set a priority for a card with no priority (Normal Flow)", ({
     given,
     when,
     then,
   }) => {
-    given("user with username Fizbin is logged into the system", () => {});
+    givenFizBinLoggedIn(given);
 
-    given("the user has one board", () => {});
+    givenUserHasOneBoard(given);
 
-    given("the user has selected that board", () => {});
+    givenBoardSelected(given);
 
-    given(/^the board has one column with name "(.*)"$/, (arg0) => {});
+    givenBoardHasColumnWithName(given);
 
-    given(
-      /^that column has one and only one card with name "(.*)"$/,
-      (arg0) => {}
-    );
+    givenColumnHasOneCardWithName(given);
 
-    given(
-      /^the card with name "(.*)" does not have a priority set$/,
-      (arg0) => {}
-    );
+    givenCardHasNullPriority(given);
 
-    when(
-      /^the user sets the priority of the card with name "(.*)" to (.*)$/,
-      (arg0, arg1) => {}
-    );
+    whenUserSetsPriorityOfCard(when);
 
-    then(/^the card with name "(.*)" has priority (.*)$/, (arg0, arg1) => {});
+    thenCardShallHaveThatPriority(then);
   });
 });
